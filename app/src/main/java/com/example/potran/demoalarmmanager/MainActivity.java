f0006d654dd79dc307f5680ce2bcc9f882503dcf
15017167 <podean0957@gmail.com>
2017-05-30 10:37:51 +0800

    updated

--- app/src/main/java/com/example/potran/demoalarmmanager/MainActivity.java
+++ app/src/main/java/com/example/potran/demoalarmmanager/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.potran.demoalarmmanager;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSet;
+    AlarmManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSet = (Button) findViewById(R.id.buttonSet);
+
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                //Create a new PendingIntent and add it to the AlarmManager
+                Intent i = new Intent(MainActivity.this, AlarmReceiverActivity.class);
+                int reqCode = 12345;
+                PendingIntent pendingI = PendingIntent.getActivity(MainActivity.this,
+                        reqCode, i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                //Get AlarmManager instance
+                am = (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+
+                //Set the alarm
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingI);
+            }
+        });
+
+
     }
 }
